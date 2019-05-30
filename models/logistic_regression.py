d8133d4d7367602032de67c789edac3834718e6f
hongwei <hongweifly@163.com>
2019-05-30 19:57:28 +0800

    more

---
+++ models/logistic_regression.py
@@ -0,0 +1,22 @@
+# -*- coding:utf-8 -*-
+""" 
+@file: logistic_regression.py 
+@time: 2019/05/30
+"""
+
+import pandas as pd
+import numpy as np
+
+
+class Model(object):
+    def __int__(self):
+        pass
+
+
+
+
+
+
+if __name__ == '__main__':
+    m = Model()
+
