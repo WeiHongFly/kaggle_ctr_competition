61f6c16494682b18810f80ab6fd7cabedd6330fc
HongWei <hongshu2016@gmail.com>
2019-06-15 17:17:01 +0800

    add sampling method

---
+++ utils/sampling.py
@@ -0,0 +1,19 @@
+"""
+加入数据的采样，通过采样，训练多个模型，进而进行多模型融合
+分层采样
+随机采样
+bagging
+"""
+
+import pandas as pd
+import numpy as np
+import random
+import os
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
